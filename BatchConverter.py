0f0817c5d8db0a5e585352423b16c3d98aa5e916
David White <whited@carleton.edu>
2017-05-11 11:29:56 -0500

    Fixed bug with audio/video conversions.

--- BatchConverter.py
+++ BatchConverter.py
@@ -155,6 +155,8 @@ def convertBatch(stuff):
 		params['extension']=params['input']
 		params['outextension']=params['output']
 		params['max_size']=int(params['max_size'])
+		if('extra_args' not in params):
+			params['extra_args']=''
 	if('usefile' in params): #Grab the relevant file names before there's any chance of modifying them
 		top="/"
 		try:
